ab6b2c3ac07579b8e35b2df5ce02c119ac0c77d1
Kolosov <skolosow.work@gmail.com>
2022-05-31 17:12:37 +0300

    laksjhdkasjhd

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ git clone https://github.com/Laradock/laradock.git
 cp .env.example .env
 cd laradock
 cp .env.example .env
-docker-compose up nginx mysql phpmyadmin redis workspace
+docker-compose up -d nginx mysql phpmyadmin redis workspace
 docker-compose exec workspace bash
 composer install

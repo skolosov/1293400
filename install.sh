99781d39a992d85cc96a1d85bb4643e9d05cd3df
Kolosov <skolosow.work@gmail.com>
2022-07-18 00:09:46 +0300

    add log

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ git clone https://github.com/Laradock/laradock.git
 cp .env.example .env
 cd laradock
 cp .env.example .env
-docker-compose up -d nginx mysql phpmyadmin redis workspace
+docker-compose up nginx mysql phpmyadmin redis workspace
